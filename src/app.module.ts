4af8bb7410c8f0cf7c11233dde28dca8eeb7424c
Hélène BAUDRAND <Helene@MacBook-Pro-de-apixis.local>
2023-10-16 15:11:29 +0200

    refactor(installed nestjs-config): refactored connexion to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
